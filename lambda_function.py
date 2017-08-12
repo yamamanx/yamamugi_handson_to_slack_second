dbd1cb2d32c747d66812af09435322549d589e0b
mitsuhiro-yamashita <yamamanx@gmail.com>
2017-08-12 14:21:38 +0900

    release

---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+
+
+import requests
+import json
+import os
+import logging
+import traceback
+
+
+slack_url = os.environ['SLACK_URL']
+
+
+def sendMessage(content, channel):
+    payload_dic = {
+        "text": content,
+        "channel": channel,
+    }
+    requests.post(slack_url, data=json.dumps(payload_dic))
+
+
+def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    try:
+        kintone_url = event['kintone_url']
+        message_template = u':telephone:申込受付レコードを作成しました\n{url}'
+        sendMessage(
+            message_template.format(url=kintone_url),
+            '#general'
+        )
+
+    except Exception as e:
+        sendMessage(traceback.format_exc(), '#general')
+        logger.error(traceback.format_exc())
+        raise(traceback.format_exc())
