54ad4fe0189b9282def8fdb82c7bf819a640889f
mitsuhiro-yamashita <yamamanx@gmail.com>
2017-08-16 20:16:36 +0900

    logger debug可能にした

--- lambda_function.py
+++ lambda_function.py
@@ -9,30 +9,39 @@ import traceback
 
 
 slack_url = os.environ['SLACK_URL']
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+
+logger = logging.getLogger()
+
+if log_level == 'ERROR':
+    logger.setLevel(logging.ERROR)
+elif log_level == 'DEBUG':
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
 
 
-def sendMessage(content, channel):
+def send_message(content, channel):
     payload_dic = {
         "text": content,
         "channel": channel,
     }
-    requests.post(slack_url, data=json.dumps(payload_dic))
-
+    logger.debug(payload_dic)
+    response = requests.post(slack_url, data=json.dumps(payload_dic))
+    logger.debug(response.text)
 
 def lambda_handler(event, context):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
 
     try:
         kintone_url = event.get('kintone_url','')
         message_template = u':telephone:申込受付レコードを作成しました\n{url}'
-        sendMessage(
+        send_message(
             message_template.format(url=kintone_url),
             '#general'
         )
         return event
 
     except Exception as e:
-        sendMessage(traceback.format_exc(), '#general')
+        send_message(traceback.format_exc(), '#general')
         logger.error(traceback.format_exc())
         raise(traceback.format_exc())
